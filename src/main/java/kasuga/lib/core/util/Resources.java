6e49ed57462ca428ed3f37eb236faeb08fe301a0
HTony03 <2761431803@qq.com>
2024-04-01 18:14:22 +0800

    翻1

--- src/main/java/kasuga/lib/core/util/Resources.java
+++ src/main/java/kasuga/lib/core/util/Resources.java
@@ -25,6 +25,11 @@ public class Resources {
      * @param isFile is the location pointing at a file or a folder?
      * @return the map of resource path and the resource.
      * @throws IOException if the file/folder don't exist, throw this.
+     * 通过路径获取资源
+     * @param location 资源的路径或文件夹
+     * @param isFile 路径是文件还是文件夹
+     * @return 资源的map路径活资源本身
+     * @throws IOException 如果文件/文件夹不存在则抛出此异常
      */
     @Util
     public static Map<String, net.minecraft.server.packs.resources.Resource> getResources(ResourceLocation location, boolean isFile) throws IOException {
@@ -38,6 +43,11 @@ public class Resources {
      * @param isFile is the location pointing at a file or a folder?
      * @return the map of resource full path and the resource.
      * @throws IOException if the file/folder don't exist, throw this.
+     * 通过绝对路径获取资源。绝对路径意味着包含资源所在的路径
+     * @param location 资源的路径或文件夹
+     * @param isFile 路径是文件还是文件夹
+     * @return 资源的map路径活资源本身
+     * @throws IOException 如果文件/文件夹不存在则抛出此异常
      */
     @Util
     public static Map<String, net.minecraft.server.packs.resources.Resource> getFullPathResources(ResourceLocation location, boolean isFile) throws IOException {
@@ -50,6 +60,10 @@ public class Resources {
      * @param location the file location.
      * @return the resource.
      * @throws IOException if the file don't exist, throw this.
+     * 从资产或数据获取资源
+     * @param location 文件位置
+     * @return 资源本身
+     * @throws IOException 如果文件不存在则抛出此异常
      */
     @Util
     public static net.minecraft.server.packs.resources.Resource getResource(ResourceLocation location) throws IOException {
@@ -61,6 +75,9 @@ public class Resources {
      * Get Block from its registration key.
      * @param location the block registration key (we usually use them in data-gen)
      * @return the block we got.
+     * 从注册键获取方块
+     * @param location 方块的注册键(在生成数据时使用)
+     * @return 获取到的方块本身
      */
     @Util
     public static Block getBlock(ResourceLocation location) {
@@ -71,6 +88,9 @@ public class Resources {
      * Get item from its registration key.
      * @param location the item registration key (we usually use them in data-gen)
      * @return the item we got.
+     * 从注册键获取物品
+     * @param location 方块的注册键(在生成数据时使用)
+     * @return 获取到的物品本身
      */
     @Util
     public static Item getItem(ResourceLocation location) {
