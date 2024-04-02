16dbd027344c1d46da6f2a5bc91db757b4f98242
HTony03 <2761431803@qq.com>
2024-04-02 18:20:28 +0800

    翻2

--- src/main/java/kasuga/lib/registrations/Reg.java
+++ src/main/java/kasuga/lib/registrations/Reg.java
@@ -6,6 +6,8 @@ import kasuga.lib.registrations.registry.SimpleRegistry;
 /**
  * Reg is the core class for KasugaLib style registration. We use this to complete most kinds of registrations.
  * For more info, see {@link SimpleRegistry}
+ * Reg是KasugaLib样式注册的核心类。我们使用这个类完成大部分类型的注册工作
+ * 更多信息请见 {@link SimpleRegistry}
  */
 public abstract class Reg {
 
@@ -20,6 +22,10 @@ public abstract class Reg {
      * any reg element. Call this, we would hand all elements in to forge and minecraft.
      * @param registry the mod SimpleRegistry.
      * @return self.
+     * 这个方法应当在所有config被给予后被调用，意味着这个方法应当在所有register以后最后使用
+     * 当这个方法被调用后，将把所有的元素提交到Forge与Minecraft中
+     * @param registry SimpleRegistry类
+     * @return 自身
      */
     @Mandatory
     public abstract Reg submit(SimpleRegistry registry);
